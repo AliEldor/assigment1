8835f09db1df689ec65c2a98f5c55da0f387b329
Ali Eldor <alieldor02@gmail.com>
2025-02-12 23:51:37 +0200

    better readability of conditions and main function

--- ex1.py
+++ ex1.py
@@ -1,12 +1,19 @@
 def TicketPriceCalculator(age):
     price=0
     
-    if age<=5:
-        price=0
-    elif 5<age<9:
-        price=5
-    elif 9<age<15:
-        price=10
-    elif age>=15:
-        price=13
+    if age <= 5:
+        price = 0
+    elif age >= 6 and age <= 8:  
+        price = 5
+    elif age >= 9 and age <= 14:
+        price = 10
+    else:  
+        price = 13
+    return price
+
+if __name__=="__main__":
+    
+    age =int(input("enter your age: "))
+    price = TicketPriceCalculator(age)
+    print(f"Your movie ticket price is: ${price}")
 

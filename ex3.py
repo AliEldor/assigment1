f287c26e991dd95423dfab8b95cea394eb0fcfc8
Ali Eldor <alieldor02@gmail.com>
2025-02-13 23:05:33 +0200

    condition

--- ex3.py
+++ ex3.py
@@ -3,4 +3,6 @@ if __name__== "__main__":
     username= input("Enter the username: ")
     password= input("Enter the password: ")
     
+    if(username=="admin" and password=="1234"):
+        print("Access Granted")
     
